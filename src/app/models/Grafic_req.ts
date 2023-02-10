4b8bd913b2a706a9c46976e4cad5c2852f0c85dd
HAMZA TRIFIS <Hamza.trifis@sofrecom.com>
2023-02-10 10:37:35 +0100

    c

---
+++ src/app/models/Grafic_req.ts
@@ -0,0 +1,14 @@
+export class Grafic_req {
+    idGrafic?: String;
+  iar?: String;
+  code_imb?: String;
+  groupe_operation?: String;
+  dateTraitement?: String;
+  statut_graphic?: String;
+  traitement_effectue?: String;
+  type_traitement?: String;
+  idactetrait?: String;
+  affectation?: String;
+  duree?: Number;
+  commentaire?: String;
+}
